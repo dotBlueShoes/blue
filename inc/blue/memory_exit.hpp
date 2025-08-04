48f59f795da0cd9f1ffc68e98671b0d3b8cdc8a5
Matthew <40423487+dotBlueShoes@users.noreply.github.com>
2025-08-04 19:08:35 +0200

    Added temporary exit memory tracker

--- inc/blue/memory_exit.hpp
+++ inc/blue/memory_exit.hpp
@@ -56,15 +56,36 @@
 
     namespace MEMORY::EXIT {
 
+        // bop/push -> pop
+
+        //  ABOUT
+        //
+        // ---  lasting  ---
+        // Elements placed on the stack will be deallocated from the newest to the oldest.
+        //  -> push() - pop()
+        //
+        // --- temporary ---
+        // Elements placed on the temporary range will be deallocated from the oldes to the newest.
+        //  When temporary range is not being used anymore we can reset it using -> clear().
+        //  -> add() - rem()
+        //  Elements added onto temporary range exist only to help create elements on the stack!
+        //  Or to keep track of elements that are ment to be deallocated mid execution.
+        //  In some cases it might be required to call clear() and add() to readd elements that are still
+        //  supposed to be tracked.
+
+        u8 backCounter = MEMORY_EXIT_SIZE;
+        u8 tempCounter = MEMORY_EXIT_SIZE;
         u8 memoryCounter = 0;
 
         using DeleteFunction = void (*) (DEALLOC_ARGS);
 
-        DeleteFunction functions [MEMORY_EXIT_SIZE];
-        void* memories	[MEMORY_EXIT_SIZE];
-        u32   sizes		[MEMORY_EXIT_SIZE];
+        DeleteFunction  functions   [MEMORY_EXIT_SIZE];
+        void*           memories    [MEMORY_EXIT_SIZE];
+        u32             sizes		[MEMORY_EXIT_SIZE];
 
         void ATEXIT () {
+            // abc -> cba memory deallocation.
+            //
             for (u8 i = memoryCounter; i != 0; --i) {
                 auto& func		= functions	[i - 1];
                 auto& memory	= memories	[i - 1];
@@ -73,23 +94,64 @@
                 auto&& _func = (DeleteFunction)func;
                 _func (size, memory);
             }
+
+            // abc -> abc memory deallocation.
+            //
+            for (u8 i = tempCounter; i < backCounter; ++i) {
+                auto& func		= functions	[i];
+                auto& memory	= memories	[i];
+                auto& size		= sizes 	[i];
+
+                auto&& _func = (DeleteFunction)func;
+                _func (size, memory);
+            }
         }
 
-        
+        // Lasting variant (elements are pushed or poped from a stack).
+        //
         void PUSH (
-            DeleteFunction function, 
-            MEMORY_TYPE size, 
-            void* memory
+            DeleteFunction  function, 
+            MEMORY_TYPE     size, 
+            void*           memory
         ) {
-            functions[memoryCounter] 	= function;
-            memories[memoryCounter] 	= memory;
-            sizes[memoryCounter] 		= size;
+            functions   [memoryCounter] = function;
+            memories    [memoryCounter] = memory;
+            sizes       [memoryCounter] = size;
 
             ++memoryCounter;
         }
 
+        // Lasting variant (elements are pushed or poped from a stack).
+        //
         void POP () {
             --memoryCounter;
+        }
+
+        // Temporary variant (elements are added)
+        //
+        void ADD (
+            DeleteFunction  function, 
+            MEMORY_TYPE     size, 
+            void*           memory
+        ) {
+            --tempCounter;
+
+            functions   [tempCounter] = function;
+            memories    [tempCounter] = memory;
+            sizes       [tempCounter] = size;
+        }
+
+        // Temporary variant 
+        //
+        void REM () {
+            --backCounter;
+        }
+
+        // Temporary variant 
+        //
+        void CLEAR () {
+            tempCounter = MEMORY_EXIT_SIZE;
+            backCounter = MEMORY_EXIT_SIZE;
         }
 
     }
